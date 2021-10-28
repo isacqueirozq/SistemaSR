21c5ea792e263d950c6007f3bd811448e0e432d1
isacqueiroz <isacqueirozq@hotmail.com>
2021-10-28 13:57:22 -0300

    Incluido avançar e voltar o dia

--- saidas_campo_lista.php
+++ saidas_campo_lista.php
@@ -220,56 +220,256 @@ function t($dia){
     <div class="container px-1 px-sm-4 py-5 mx-auto">
         <div class="row d-flex justify-content-center">
             <div class="card text-center pt-4 border-0">
+                <!-- VISOR -->
                 <div id="display">
                     <?php
-                    $dia = date('N'); // 1 para segunda e 7 para domingo
-                    echo "<h4 class='mb-0'>".t($dia)[0]."</h4> <small class='text-muted mb-3'>Dirigente</small>
-                    <small>
-                        <h5>".t($dia)[1]."</h5>
-                    </small>
-                    <h2 class='large-font'>".t($dia)[2]."</h2>
-                    <small>
-                        <a href='".t($dia)[3]."'>LINK DA CONSIDERAÇÃO VIA ZOOM</a>
-                    </small>
-                    <div class='text-center mt-3 mb-4'>
-                        <!-- informações -->
-                    </div>"
+                    function padrao($dia){
+                        if ($dia == "") {
+                            $dia = date('N'); // 1 para segunda e 7 para domingo
+                        }
+                    
+                        $today = date('N');
+                            if ($dia == $today) {
+                                echo "<p class='caption-container'>HOJE</p>";
+                            }
+                        echo "<h4 class='mb-0'>".t($dia)[0]."</h4> <small class='text-muted mb-3'>Dirigente</small>
+                        <small>
+                            <h5>".t($dia)[1]."</h5>
+                        </small>
+                        <h2 class='large-font'>".t($dia)[2]."</h2>
+                        <small>
+                            <a href='".t($dia)[3]."'>LINK DA CONSIDERAÇÃO VIA ZOOM</a>
+                        </small>
+                        <div class='text-center mt-3 mb-4'>
+                            <!-- informações -->
+                        </div>";
+                    }
+                    
+                    // padrao(0);
                     ?>
-                </div>
+                    
+                    <!-- **** Slide ***  -->
+                    <!-- ESTILO -->
+                    <style>
+                        * {
+                        box-sizing: border-box;
+                        }
 
+                        /* Position the image container (needed to position the left and right arrows) */
+                        .container {
+                        position: relative;
+                        }
 
-            <div class="linha d-flex px-3 mt-auto">
-                
-                <div class="d-flex flex-column block first-block">      <small class="text-muted mb-0">SEG</small>
-                    <div class="text-center"><img class="symbol-img" src="https://image.flaticon.com/icons/png/512/609/609040.png"></div>
-                    <h6><strong>18:00</strong></h6>
-                </div>
-                <div class="d-flex flex-column block"> <small class="text-muted mb-0">TER</small>
-                    <div class="text-center"><img class="symbol-img" src="https://image.flaticon.com/icons/png/512/609/609040.png"></div>
-                    <h6><strong>18:00</strong></h6>
-                </div>
-                <div class="d-flex flex-column block active"> <small class="text-muted mb-0">QUA</small>
-                    <div class="text-center"><img class="symbol-img" src="https://image.flaticon.com/icons/png/512/609/609040.png"></div>
-                    <h6><strong>18:00</strong></h6>
-                </div>
-                <div class="d-flex flex-column block"> <small class="text-muted mb-0">QUI</small>
-                    <div class="text-center"><img class="symbol-img" src="https://image.flaticon.com/icons/png/512/1147/1147965.png"></div>
-                    <h6><strong>-</strong></h6>
-                </div>
-                <div class="d-flex flex-column block"> <small class="text-muted mb-0">SEX</small>
-                    <div class="text-center"><img class="symbol-img" src="https://image.flaticon.com/icons/png/512/609/609040.png"></div>
-                    <h6><strong>18:00</strong></h6>
-                </div>
-                <div class="d-flex flex-column block"> <small class="text-muted mb-0">SAB</small>
-                    <div class="text-center"><img class="symbol-img" src="https://image.flaticon.com/icons/png/512/609/609040.png"></div>
-                    <h6><strong>8:30</strong></h6>
-                </div>
-                <div class="d-flex flex-column block last-block"> <small class="text-muted mb-0">DOM</small>
-                    <div class="text-center"><img class="symbol-img" src="https://image.flaticon.com/icons/png/512/609/609040.png"></div>
-                    <h6><strong>8:30</strong></h6>
-                </div>
+                        /* Hide the images by default */
+                        .mySlides {
+                        display: none;
+                        }
 
-            </div>
+                        /* Add a pointer when hovering over the thumbnail images */
+                        .cursor {
+                        cursor: pointer;
+                        }
+
+                        /* Next & previous buttons */
+                        .prev,
+                        .next {
+                        cursor: pointer;
+                        position: absolute;
+                        top: 40%;
+                        width: auto;
+                        padding: 16px;
+                        margin-top: -50px;
+                        color: white;
+                        font-weight: bold;
+                        font-size: 20px;
+                        border-radius: 0 3px 3px 0;
+                        user-select: none;
+                        -webkit-user-select: none;
+                        }
+
+                        /* Position the "next button" to the right */
+                        .next {
+                        right: 0;
+                        border-radius: 3px 0 0 3px;
+                        }
+                        .prev {
+                        left: 0;
+                        border-radius: 3px 0 0 3px;
+                        }
+
+                        /* On hover, add a black background color with a little bit see-through */
+                        .prev:hover,
+                        .next:hover {
+                        background-color: rgba(0, 0, 0, 0.8);
+                        }
+
+                        /* Number text (1/3 etc) */
+                        .numbertext {
+                        color: #f2f2f2;
+                        font-size: 12px;
+                        padding: 8px 12px;
+                        position: absolute;
+                        top: 0;
+                        display: none;
+                        }
+
+                        /* Container for image text */
+                        .caption-container {
+                        text-align: center;
+                        background-color:indigo;
+                        padding: 2px 16px;
+                        color: white;
+                        }
+
+                        .row:after {
+                        content: "";
+                        display: table;
+                        clear: both;
+                        }
+
+                        /* Six columns side by side */
+                        .column {
+                        float: left;
+                        width: 16.66%;
+                        }
+
+                        /* Add a transparency effect for thumnbail images */
+                        .demo {
+                        opacity: 0.6;
+                        }
+
+                        .active,
+                        .demo:hover {
+                        opacity: 1;
+                        }
+                    </style>
+                    <!-- PÁGINAS -->
+                    <!-- Container for the image gallery -->
+                    <div class="container">
+                        <!-- Full-width images with number text -->
+                        <div class="mySlides">
+                            <!-- numbertext está sendo oculto no css -->
+                            <div class="numbertext">0 / 0</div> 
+                            <?php padrao(0);?>
+                        </div>
+
+                        <div class="mySlides">
+                            <div class="numbertext">1 / 7</div>
+                            <?php padrao(1);?>
+                        </div>
+
+                        <div class="mySlides">
+                            <div class="numbertext">2 / 7</div>
+                            <?php padrao(2);?>
+                        </div>
+
+                        <div class="mySlides">
+                            <div class="numbertext">3 / 7</div>
+                            <?php padrao(3);?>
+                        </div>
+
+                        <div class="mySlides">
+                            <div class="numbertext">4 / 7</div>
+                            <?php padrao(4);?>
+                        </div>
+
+                        <div class="mySlides">
+                            <div class="numbertext">5 / 7</div>
+                            <?php padrao(5);?>
+                        </div>
+
+                        <div class="mySlides">
+                            <div class="numbertext">6 / 7</div>
+                            <?php padrao(6);?>
+                        </div>
+
+                        <div class="mySlides">
+                            <div class="numbertext">7 / 7</div>
+                            <?php padrao(7);?>
+                        </div>
+
+                        <!-- Next and previous buttons -->
+                        <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+                        <a class="next" onclick="plusSlides(1)">&#10095;</a>
+
+                        <!-- Image text -->
+                        <div class="caption-container" hidden>
+                            <p id="caption"></p>
+                        </div>
+
+                        <script>
+                            var slideIndex = 1;
+                            showSlides(slideIndex);
+
+                            // Next/previous controls
+                            function plusSlides(n) {
+                            showSlides(slideIndex += n);
+                            }
+
+                            // Thumbnail image controls
+                            function currentSlide(n) {
+                            showSlides(slideIndex = n);
+                            }
+
+                            function showSlides(n) {
+                            var i;
+                            var slides = document.getElementsByClassName("mySlides");
+                            var dots = document.getElementsByClassName("demo");
+                            var captionText = document.getElementById("caption");
+                            if (n > slides.length) {slideIndex = 1}
+                            if (n < 1) {slideIndex = slides.length}
+                            for (i = 0; i < slides.length; i++) {
+                                slides[i].style.display = "none";
+                            }
+                            for (i = 0; i < dots.length; i++) {
+                                dots[i].className = dots[i].className.replace(" active", "");
+                            }
+                            slides[slideIndex-1].style.display = "block";
+                            dots[slideIndex-1].className += " active";
+                            captionText.innerHTML = dots[slideIndex-1].alt;
+                            } 
+                        </script>
+                        <!-- Fim Slide -->
+                    </div>
+                </div>
+                <!-- MENU INFERIOR -->
+                <div class="linha d-flex px-3 mt-auto">
+                    <div hidden>
+                        <div class="d-flex flex-column block first-block demo">      <small class="text-muted mb-0">HOJE</small>
+                            <div class="text-center" onclick="currentSlide(1)"><img class="symbol-img" src="https://image.flaticon.com/icons/png/512/609/609040.png"></div>
+                            <h6><strong>18:00</strong></h6>
+                        </div>
+                    </div>
+                    <div class="d-flex flex-column block first-block demo">      <small class="text-muted mb-0">SEG</small>
+                        <div class="text-center" onclick="currentSlide(2)"><img class="symbol-img" src="https://image.flaticon.com/icons/png/512/609/609040.png"></div>
+                        <h6><strong>18:00</strong></h6>
+                    </div>
+                    <div class="d-flex flex-column block demo"> <small class="text-muted mb-0">TER</small>
+                        <div class="text-center" onclick="currentSlide(3)"><img class="symbol-img" src="https://image.flaticon.com/icons/png/512/609/609040.png"></div>
+                        <h6><strong>18:00</strong></h6>
+                    </div>
+                    <div class="d-flex flex-column block demo"> <small class="text-muted mb-0">QUA</small>
+                        <div class="text-center" onclick="currentSlide(4)"><img class="symbol-img" src="https://image.flaticon.com/icons/png/512/609/609040.png"></div>
+                        <h6><strong>18:00</strong></h6>
+                    </div>
+                    <div class="d-flex flex-column block demo"> <small class="text-muted mb-0">QUI</small>
+                        <div class="text-center" onclick="currentSlide(5)"><img class="symbol-img" src="https://image.flaticon.com/icons/png/512/1147/1147965.png"></div>
+                        <h6><strong>-</strong></h6>
+                    </div>
+                    <div class="d-flex flex-column block demo"> <small class="text-muted mb-0">SEX</small>
+                        <div class="text-center" onclick="currentSlide(6)"><img class="symbol-img" src="https://image.flaticon.com/icons/png/512/609/609040.png"></div>
+                        <h6><strong>18:00</strong></h6>
+                    </div>
+                    <div class="d-flex flex-column block demo"> <small class="text-muted mb-0">SAB</small>
+                        <div class="text-center" onclick="currentSlide(7)"><img class="symbol-img" src="https://image.flaticon.com/icons/png/512/609/609040.png"></div>
+                        <h6><strong>8:30</strong></h6>
+                    </div>
+                    <div class="d-flex flex-column block last-block demo"> <small class="text-muted mb-0">DOM</small>
+                        <div class="text-center" onclick="currentSlide(8)"><img class="symbol-img" src="https://image.flaticon.com/icons/png/512/609/609040.png"></div>
+                        <h6><strong>8:30</strong></h6>
+                    </div>
+
+                </div>
             </div>
         </div>
     </div>
